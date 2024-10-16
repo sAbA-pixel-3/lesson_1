b62e79e6e2612187170e0d035d5e0c60a8e95500
sAbA-pixel-3 <seconddonutbs@gmail.com>
2024-10-16 18:11:44 +0600

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,49 @@
+# print('qwerty') # оператор вывода на экран
+
+
+# name = 'jhon'
+# mum = 33 # = это оператор присваивания
+# print(name, mum)
+
+
+# num = 33
+# num1 = 34
+# result = num1+num
+# print(result) # -вычетание , /деление, *умножение, 
+
+
+# name = 'jhon'
+# last_name = 'qwert'
+# print(f'hello my name is {name} {last_name} tyuhbdcb') # f - эф строка (форматирование строк)
+
+
+# n = '10' # int=integer, число
+# f = 'hello' # str=string, строка
+# print(n+f) # must be "int+int" or "str+str"
+
+
+# name = input('Enter name: ') # оператор ввода через экран "cmd"
+# print(f'Hello {name},\nHow are you?') # \n= начиннает с новой строки
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
